f13416b5e95e6f980181c83a0094fc61f0cc0360
Vaibhav Rajpoot <vaibhavrajpoot000.com>
2024-03-27 22:01:11 +0530

    initial commit

---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace DataDashboard.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
